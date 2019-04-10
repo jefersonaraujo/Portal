b4f15849aa65ac9210336230580f52919df40271
Jeferson Araujo <jefersonaraujo95@gmail.com>
2019-04-10 15:10:55 -0300

    Let's go

--- htools/app/Concentrador.php
+++ htools/app/Concentrador.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Concentrador extends Model
 {
-    //
+    protected $table = 'concentrador';
+    protected $primarykey = 'cod_concentrador';
+
+    public $timestamps = false;
+    protected $fillable = [
+
+      'descricao',
+      'ip',
+      'api_port',
+      'ssh_port',
+      'snmp_port',
+      'snmp',
+      'latitude',
+      'altitude',
+      'user',
+      'password'
+
+    ];
+
+    protected $guarded[];
+
 }
