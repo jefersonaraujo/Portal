d4070ddbe573582a3d25a930baff6bdc152eee74
Jeferson Araujo <jefersonaraujo95@gmail.com>
2019-04-10 11:28:41 -0300

    let

--- htools/resources/views/welcome.blade.php
+++ htools/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Htools
                 </div>
 
                 <div class="links">
