939d0c99824b29123c9a0e9859970b6f35dc452f
Jeferson Araujo <jefersonaraujo95@gmail.com>
2019-04-17 16:03:19 -0300

    modification route

--- htools/routes/web.php
+++ htools/routes/web.php
@@ -11,12 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::resource('cadastro/concentrador', 'ConcentradorController');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
