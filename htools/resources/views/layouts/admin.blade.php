88d0b9f8220b81b5264f719ef384c44c4a29d7ee
Jeferson Araujo <jefersonaraujo95@gmail.com>
2019-04-17 12:11:41 -0300

    add

--- htools/resources/views/layouts/admin.blade.php
+++ htools/resources/views/layouts/admin.blade.php
@@ -204,5 +204,12 @@
     <!-- AdminLTE App -->
     <script src="{{asset('js/app.min.js')}}"></script>
 
+    <!-- <script src="{{asset('js/jquery.min.js')}}"></script> -->
+
+    <link rel="stylesheet" href="
+http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
+<script src="{{ asset( 'js/jquery.ui.datepicker-es.js ') }}"></script>
   </body>
 </html>
