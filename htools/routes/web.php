36cf85c2f179b55b3a6477b166369c053d1aa208
Jeferson Araujo <jefersonaraujo95@gmail.com>
2019-04-17 10:31:03 -0300

    add auth

--- htools/routes/web.php
+++ htools/routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::resource('cadastro/concentrador', 'ConcentradorController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
