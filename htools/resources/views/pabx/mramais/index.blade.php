25bb67dce4b276561c1e65f6ab7e28ad38ae3c58
Jeferson Araujo <jefersonaraujo95@gmail.com>
2019-04-27 11:32:24 -0300

    monitor

--- htools/resources/views/pabx/mramais/index.blade.php
+++ htools/resources/views/pabx/mramais/index.blade.php
@@ -1,37 +1,70 @@
 @extends('layouts.admin')
 @section('conteudo')
-<div class="row">
-	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-		<h3>Lista de Categorias <a href="concentrador/create"><button class="btn btn-success">Novo</button></a></h3>
 
-	</div>
-</div>
+    <div class="info-box">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
 
-<div class="row">
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-		<div class="table-responsive">
-			<table class="table table-striped table-bordered table-condensed table-hover">
-				<thead>
-					<th>Id</th>
-					<th>Id_agent</th>
-					<th>Telefone</th>
-          <th>inicial</th>
-          <th>final</th>
-          <th>duração</th>
-          <th>status</th>
-          <th>wait</th>
+              <div class="info-box-content">
+                  <span class="info-box-text">CPU Traffic</span>
+                  <span class="info-box-number">90<small>%</small></span>
+              </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+    <!-- /.col -->
+    <div class="col-md-3 col-sm-6 col-xs-12">
+      <div class="info-box">
+        <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
 
-				</thead>
+        <div class="info-box-content">
+          <span class="info-box-text">Likes</span>
+          <span class="info-box-number">41,410</span>
+        </div>
+      <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
 
-        <tr>
+    <!-- fix for small devices only -->
+    <div class="clearfix visible-sm-block"></div>
 
+    <div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="info-box">
+      <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
 
-        </tr>
-
-    
-      </table>
+      <div class="info-box-content">
+        <span class="info-box-text">Sales</span>
+        <span class="info-box-number">760</span>
       </div>
+      <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="info-box">
+      <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
 
- </div>
- </div>
- @stop
+      <div class="info-box-content">
+        <span class="info-box-text">New Members</span>
+        <span class="info-box-number">2,000</span>
+      </div>
+      <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </div>
+
+
+
+
+
+@endsection
