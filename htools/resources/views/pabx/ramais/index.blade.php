f7d1ca41dd4dfea21c6d85dbb3501491fba3f61d
Jeferson Araujo <jefersonaraujo95@gmail.com>
2019-04-29 17:35:29 -0300

    layout

--- htools/resources/views/pabx/ramais/index.blade.php
+++ htools/resources/views/pabx/ramais/index.blade.php
@@ -2,7 +2,7 @@
 @section('conteudo')
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-		<h3>Lista de Categorias <a href="concentrador/create"><button class="btn btn-success">Novo</button></a></h3>
+		<h3>Lista de Ramais <a href="concentrador/create"><button class="btn btn-success">Novo</button></a></h3>
 		@include('pabx.ramais.search')
 	</div>
 </div>
