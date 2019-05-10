545b2dd86098322874b6fdae4747381c308d7e1b
Jeferson Araujo <jefersonaraujo95@gmail.com>
2019-05-10 10:45:42 -0300

    add plug

--- htools/resources/views/layouts/admin.blade.php
+++ htools/resources/views/layouts/admin.blade.php
@@ -13,7 +13,14 @@
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('css/AdminLTE.min.css')}}">
     <link rel="stylesheet" href="{{asset('plugins/Ionicons/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap-daterangepicker/daterangepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/timepicker/bootstrap-timepicker.min.css')}}">
+
     <link rel="stylesheet" href="{{asset('plugins/jvectormap/jquery-jvectormap.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/iCheck/all.css')}}">
 
     <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
@@ -214,6 +221,90 @@
     <script src="{{asset('plugins/chart.js/Chart.js')}}"></script>
     <script src="{{asset('plugins/fastclick/fastclick.js')}}"></script>
     <script src="{{asset('plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+    <script src="{{asset('plugins/input-mask/jquery.inputmask.js')}}"></script>
+    <script src="{{asset('plugins/input-mask/jquery.inputmask.date.extensions.js')}}"></script>
+    <script src="{{asset('plugins/input-mask/jquery.inputmask.extensions.js')}}"></script>
+
+    <script src="{{asset('plugins/timepicker/bootstrap-timepicker.min.js')}}"></script>
+
+
+    <link rel="stylesheet" href="{{asset('bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js')}}">
+    <link rel="stylesheet" href="{{asset('moment/min/moment.min.js')}}">
+
+
+
+
+
+
+    <script>
+      $(function () {
+        //Initialize Select2 Elements
+        $('.select2').select2()
+
+        //Datemask dd/mm/yyyy
+        $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
+        //Datemask2 mm/dd/yyyy
+        $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
+        //Money Euro
+        $('[data-mask]').inputmask()
+
+        //Date range picker
+        $('#reservation').daterangepicker()
+        //Date range picker with time picker
+        $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A' })
+        //Date range as a button
+        $('#daterange-btn').daterangepicker(
+          {
+            ranges   : {
+              'Today'       : [moment(), moment()],
+              'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+              'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+              'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+              'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+              'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            },
+            startDate: moment().subtract(29, 'days'),
+            endDate  : moment()
+          },
+          function (start, end) {
+            $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+          }
+        )
+
+        //Date picker
+        $('#datepicker').datepicker({
+          autoclose: true
+        })
+
+        //iCheck for checkbox and radio inputs
+        $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+          checkboxClass: 'icheckbox_minimal-blue',
+          radioClass   : 'iradio_minimal-blue'
+        })
+        //Red color scheme for iCheck
+        $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+          checkboxClass: 'icheckbox_minimal-red',
+          radioClass   : 'iradio_minimal-red'
+        })
+        //Flat red color scheme for iCheck
+        $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+          checkboxClass: 'icheckbox_flat-green',
+          radioClass   : 'iradio_flat-green'
+        })
+
+        //Colorpicker
+        $('.my-colorpicker1').colorpicker()
+        //color picker with addon
+        $('.my-colorpicker2').colorpicker()
+
+        //Timepicker
+        $('.timepicker').timepicker({
+          showInputs: false
+        })
+      })
+    </script>
 
 
   </body>
--- htools/resources/views/pabx/chamadas/search.blade.php
+++ htools/resources/views/pabx/chamadas/search.blade.php
@@ -9,4 +9,19 @@
 	</div>
 </div>
 
+<!-- Date and time range -->
+<div class="form-group">
+	<label>Date range button:</label>
+
+	<div class="input-group">
+		<button type="button" class="btn btn-default pull-right" id="daterange-btn">
+			<span>
+				<i class="fa fa-calendar"></i> Date range picker
+			</span>
+			<i class="fa fa-caret-down"></i>
+		</button>
+	</div>
+</div>
+<!-- /.form group -->
+
 {{Form::close()}}
