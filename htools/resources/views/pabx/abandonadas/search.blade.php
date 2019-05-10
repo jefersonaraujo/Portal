3402ff220f56d4c25120028e097a4073a6c6674a
Jeferson Araujo <jefersonaraujo95@gmail.com>
2019-05-10 13:28:53 -0300

    search layout

---
+++ htools/resources/views/pabx/abandonadas/search.blade.php
@@ -0,0 +1,69 @@
+{!!Form::open(array('url'=>'pabx/abandonadas', 'method'=>'GET', 'autocomplete'=>'off', 'role' => 'search'))!!}
+
+<div class="row">
+	<div class="col-md-6">
+
+		<div class="box box-danger">
+			<div class="box-body">
+
+
+
+				<!-- Date range -->
+				<div class="form-group">
+					<label>Do dia:</label>
+
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-calendar"></i>
+						</div>
+						<input type="text" class="form-control pull-right" id="reservation">
+					</div>
+					<!-- /.input group -->
+				</div>
+				<!-- /.form group -->
+				<div class="form-group">
+					<label>Até:</label>
+
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-calendar"></i>
+						</div>
+						<input type="text" class="form-control pull-right" id="reservation2">
+					</div>
+					<!-- /.input group -->
+				</div>
+				<!-- /.form group -->
+
+			</div>
+			<!-- /.box-body -->
+		</div>
+
+		<div class="form-group">
+			<div class="input-group">
+				<div class="input-group-addon">
+					<i class="fa fa-phone"></i>
+				</div>
+				<input type="text" class="form-control" name="searchText" placeholder="Telefone..." value="{{$searchText}}">
+			   	<span class="input-group-btn">
+					<button type="submit" class="btn btn-primary">Buscar</button>
+				</span>
+			</div>
+		</div>
+	</div>
+	<!-- /.col (left) -->
+
+	<!-- /.col (right) -->
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+{{Form::close()}}
