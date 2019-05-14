8e4e6bfabae2612dff0ca0198611fa4bdec9a296
Jeferson Araujo <jefersonaraujo95@gmail.com>
2019-05-14 08:48:39 -0300

    update date format

--- htools/resources/views/pabx/chamadas/search.blade.php
+++ htools/resources/views/pabx/chamadas/search.blade.php
@@ -16,7 +16,7 @@
 						<div class="input-group-addon">
 							<i class="fa fa-calendar"></i>
 						</div>
-						<input type="text" class="form-control pull-right" data-date-format='dd-mm-yyyy' name="from" id="from">
+						<input type="text" class="form-control pull-right" data-date-format='dd-mm-yyyy' name="from" id="from" value="2019-05-01">
 					</div>
 					<!-- /.input group -->
 				</div>
@@ -28,7 +28,7 @@
 						<div class="input-group-addon">
 							<i class="fa fa-calendar"></i>
 						</div>
-						<input type="text" class="form-control pull-right"  data-date-format='dd-mm-yyyy'  name="to" id="to">
+						<input type="text" class="form-control pull-right"  data-date-format='dd-mm-yyyy'  name="to" id="to" value="2019-05-01">
 					</div>
 					<!-- /.input group -->
 				</div>
